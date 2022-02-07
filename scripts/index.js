88993066a51118fd41491acb98d647cd220288a1
raphaelz3 <raphael-nascimento@hotmail.com>
2022-02-06 22:23:40 -0300

    implementação LocalStorage da aréa em serviço

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,31 @@ var Servico = [];
 
 //recupera lista de espera no carregamento
 onload = function carregamento(){
-    document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Lista Vazia' +'</tr>';
-    var local = JSON.parse(localStorage.getItem("disponiveis"));
+    document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Ninguem disponivel' +'</tr>';
+    document.getElementById("filaServico").innerHTML = '<tr>'+'<td colspan="4"> Ninguem em serviço' +'</tr>';
+    var carregardisponiveis = JSON.parse(localStorage.getItem("disponiveis"));
 
-    for(c = 0; c < local.length; c++){
-        Disponiveis.push(local[c]);
+    for(let c = 0; c < carregardisponiveis.length; c++){
+        Disponiveis.push(carregardisponiveis[c]);
     }
-    if(local != null || local.length > 0){
+    if(carregardisponiveis != null || carregardisponiveis.length > 0){
         listar();
+
     }
-    console.log(local);
+    var carregarservico = JSON.parse(localStorage.getItem("emServico"));
+
+    for(let i = 0; i < carregarservico.length; i++){
+        Servico.push(carregarservico[i]);
+    }
+    if(carregarservico != null || carregarservico.length > 0){
+        listarServico();
+
+    }
+    //console.log(local);
 }
 
 //função para adicionar e formatar funcionario inserido na fila
-function adicionar(){
+function adicionarDisponiveis(){
     var funci = document.getElementById("funcionario").value;
     let data = new Date();
     let hours = data.getHours();
@@ -41,7 +52,7 @@ function adicionar(){
     if(funci != "" && funci != " "){
     //Criar objeto e adicionar ao Disponiveis(array localstorage) e salva no local storage
         Disponiveis.push({id:id, nome:funci, hora:horaExata});
-        DisponiveisString = JSON.stringify(Disponiveis)
+        DisponiveisString = JSON.stringify(Disponiveis);
         localStorage.setItem("disponiveis", DisponiveisString);
         listar();
     }else{
@@ -54,21 +65,17 @@ function adicionar(){
 function listar(){
     document.getElementById("filaEspera").innerHTML = '';
 
-    for(i = 0; i < Disponiveis.length; i++){
-        document.getElementById("filaEspera").innerHTML +=  `<tr id ="${Disponiveis[i].id}">` + '<td id="nomeFunci">' + Disponiveis[i].nome + '</td>' + '<td id="horaChegada">' + Disponiveis[i].hora + '</td>' + '<td>' + `<button class="chamar" onclick="chamar(${Disponiveis[i].id})">Chamar</button>` +'</td>' + '<td>' +`<button nome="excluir" class="excluir" onclick="deletar(${Disponiveis[i].id})">Excluir</button>` + "</td>" +'</tr>';
+    for(let i = 0; i < Disponiveis.length; i++){
+        document.getElementById("filaEspera").innerHTML += `<tr id ="${Disponiveis[i].id}">` + '<td id="nomeFunci">' + Disponiveis[i].nome + '</td>' + '<td id="horaChegada">' + Disponiveis[i].hora + '</td>' + '<td>' + `<button class="chamar" onclick="chamar(${Disponiveis[i].id})">Chamar</button>` +'</td>' + '<td>' +`<button nome="excluir" class="excluir" onclick="deletar(${Disponiveis[i].id})">Excluir</button>` + "</td>" +'</tr>';
     }
 }
 
-//contar posição requisitada e retornar a posição que elemento se encontra
-function contadordeposicao(idposicao){
-    let contador;
-    let pos = Disponiveis.map(function(e) { return e.id; });
-    for(let z = 0; z < pos.length; z++){
-        if(pos[z] == idposicao){
-            contador = z;
-        }
+function listarServico(){
+    //document.getElementById("filaServico").innerHTML = '';
+
+    for(let count = 0; count < Servico.length; count++){
+        document.getElementById("filaServico").innerHTML += `<tr id="${Servico[count].id}">` + '<td>' + Servico[count].nome + '</td>' + '<td>' + Servico[count].local+ '</td>' + '<td>' + `<button style="width:58px;" class="chamar" onclick="">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletarAtivo(${Servico[count].id})">Excluir</button>` + "</td>" +'</tr>';
     }
-    return contador
 }
 
 //abre coluna para inserir local com opções de salvar ou cancelar. Cancelar faz o reload da tela
@@ -79,25 +86,31 @@ function chamar(idchamada){
     let posicaochamada = contadordeposicao(idchamada);
     let nomeChamado = Disponiveis[posicaochamada].nome;
     let idchamadamod = idchamada + 1;
-    document.getElementById("dadosChamar").innerHTML = `<p>Funcionario: ${nomeChamado}</p>` + '<p>Insira o CEP: </p>'+`<input id="${idchamadamod}" class="inputInserirDados" type="text">` + '<br>' + `<button id="salvarChamarLocal" class="chamar" onclick="salvarChamar(${idchamadamod})">` + 'Salvar</button>' + `<button id="cancelarChamarLocal" class="excluir" onclick="cancelarChamar(${idchamadamod})"">`+ 'Cancelar</button>';
+    document.getElementById("dadosChamar").innerHTML = `<p>Funcionario: ${nomeChamado}</p>` + '<p>Insira o Endereço: </p>'+`<input id="${idchamadamod}" class="inputInserirDados" type="text">` + '<br>' + `<button id="salvarChamarLocal" class="chamar" onclick="salvarChamar(${idchamadamod})">` + 'Salvar</button>' + `<button id="cancelarChamarLocal" class="excluir" onclick="cancelarChamar(${idchamadamod})"">`+ 'Cancelar</button>';
+    console.log(nomeChamado, idchamadamod);
 }
 
 //Salva as informações inseridas e recupera o nome para armazenar na coluna de funcionarios em serviço
-//salCham é igual o id do objeto + 1 para evitar duplicar id no HTML.
-function salvarChamar(salCham){
-    let local = document.getElementById(salCham).value
-    let posicaochamada = contadordeposicao(salCham - 1);
+//iddochamado é igual o id do objeto + 1 para evitar duplicar id no HTML.
+function salvarChamar(iddochamado){
+    let local = document.getElementById(iddochamado).value;
+    let posicaochamada = contadordeposicao(iddochamado - 1);
     let nomeChamado = Disponiveis[posicaochamada].nome;
 
-    //deletar(salCham - 1)
-    document.getElementById('filaServico').innerHTML += `<tr id="${salCham}">` + `<td>${nomeChamado}</td>`+ `<td>${local}</td>` + '<td>' + `<button style="width:58px;" class="chamar" onclick="">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletar(${salCham})">Excluir</button>` + "</td>" +'</tr>';
-    deletar(salCham - 1);
+    //deletar(iddochamado - 1)
+    //document.getElementById('filaServico').innerHTML += `<tr id="${iddochamado}">` + `<td>${nomeChamado}</td>`+ `<td>${local}</td>` + '<td>' + `<button style="width:58px;" class="chamar" onclick="">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletarAtivo(${iddochamado})">Excluir</button>` + "</td>" +'</tr>';
+    deletar(iddochamado - 1);
     document.body.style="overflow: auto;";
     let exibirJanela = document.getElementById("containerChamar")
     exibirJanela.style="display: none;"
+
+    Servico.push({id:iddochamado, nome:nomeChamado, local:local})
+    ServicoString = JSON.stringify(Servico)
+    localStorage.setItem("emServico", ServicoString);
+    listarServico();
 }
 
-function cancelarChamar(cancCham){
+function cancelarChamar(){
     document.body.style="overflow: auto;";
     let exibirJanela = document.getElementById("containerChamar")
     exibirJanela.style="display: none;"
@@ -131,4 +144,16 @@ function limparFila(){
     }else{
         alert("Ação Cancelada")
     }
+}
+
+//contar posição requisitada e retornar a posição que elemento se encontra
+function contadordeposicao(idposicao){
+    let contador;
+    let pos = Disponiveis.map(function(e) { return e.id; });
+    for(let z = 0; z < pos.length; z++){
+        if(pos[z] == idposicao){
+            contador = z;
+        }
+    }
+    return contador
 }
