318432efdeb8539884ddb51fe568eecc60455be0
raphaelz3 <raphael-nascimento@hotmail.com>
2022-02-27 15:42:11 -0300

    atualização implementação da função voltar fila em serviço

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,35 @@
-//array para recebimento de objetos
+//array para recebimento de objetos do local storage
 var Disponiveis = [];
 var Servico = [];
 
 //recupera lista de espera no carregamento
-onload = function carregamento(){
-    document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Ninguem disponivel' +'</tr>';
-    document.getElementById("filaServico").innerHTML = '<tr>'+'<td colspan="4"> Ninguem em serviço' +'</tr>';
-    var carregardisponiveis = JSON.parse(localStorage.getItem("disponiveis"));
+window.addEventListener("load", function carregamentoAbaEspera(){
+
+    document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Ninguem Disponivel' +'</tr>';
+    let carregardisponiveis = JSON.parse(localStorage.getItem("disponiveis"));
 
     for(let c = 0; c < carregardisponiveis.length; c++){
         Disponiveis.push(carregardisponiveis[c]);
     }
     if(carregardisponiveis != null || carregardisponiveis.length > 0){
         listar();
-
     }
-    var carregarservico = JSON.parse(localStorage.getItem("emServico"));
+}, false)
 
-    for(let i = 0; i < carregarservico.length; i++){
-        Servico.push(carregarservico[i]);
+window.addEventListener("load", function carregamentoAbaServico(){
+    document.getElementById("filaServico").innerHTML = '<tr>'+'<td colspan="4"> Ninguem em Serviço' +'</tr>';
+    let carregarservico = JSON.parse(localStorage.getItem("localemServico"));
+    for(let w = 0; w < carregarservico.length; w++){
+        Servico.push(carregarservico[w]);
     }
-    if(carregarservico != null || carregarservico.length > 0){
+    if(carregarservico != null && carregarservico.length > 0){
         listarServico();
-
     }
-    //console.log(local);
-}
+}, false);
 
 //função para adicionar e formatar funcionario inserido na fila
-function adicionarDisponiveis(){
-    var funci = document.getElementById("funcionario").value;
+function adicionarDisponiveis(nomeVolta){
+    let funci = document.getElementById("funcionario").value;
     let data = new Date();
     let hours = data.getHours();
     let minutes = data.getMinutes();
@@ -49,7 +49,10 @@ function adicionarDisponiveis(){
     }
     horaExata = hours + ":" + minutes + ":" + seconds + "";
 
-    if(funci != "" && funci != " "){
+    if(funci == "" || funci == null){
+        funci = nomeVolta;
+    }
+    if(funci != "" && funci != " " && funci != null){
     //Criar objeto e adicionar ao Disponiveis(array localstorage) e salva no local storage
         Disponiveis.push({id:id, nome:funci, hora:horaExata});
         DisponiveisString = JSON.stringify(Disponiveis);
@@ -71,14 +74,32 @@ function listar(){
 }
 
 function listarServico(){
-    //document.getElementById("filaServico").innerHTML = '';
+    document.getElementById("filaServico").innerHTML = '';
 
     for(let count = 0; count < Servico.length; count++){
-        document.getElementById("filaServico").innerHTML += `<tr id="${Servico[count].id}">` + '<td>' + Servico[count].nome + '</td>' + '<td>' + Servico[count].local+ '</td>' + '<td>' + `<button style="width:58px;" class="chamar" onclick="">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletarAtivo(${Servico[count].id})">Excluir</button>` + "</td>" +'</tr>';
+        document.getElementById("filaServico").innerHTML += `<tr id="${Servico[count].id}">` + '<td>' + Servico[count].nome + '</td>' + '<td>' + Servico[count].local+ '</td>' + '<td>' + `<button style="width:58px;" class="chamar" onclick="voltarFila(${Servico[count].id})">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletarAtivo(${Servico[count].id})">Excluir</button>` + "</td>" +'</tr>';
     }
 }
 
-//abre coluna para inserir local com opções de salvar ou cancelar. Cancelar faz o reload da tela
+function voltarFila(volta){
+    let identificacao = Servico.map(function(e) { return e.id; });
+    let nome;
+    let compara = 0
+    for(compara = 0; compara < Servico.length; compara++){
+        if(identificacao[compara] == volta){
+            nome = Servico[compara].nome;
+            console.log(nome)
+            adicionarDisponiveis(nome)
+            deletarAtivo(volta)
+        }
+    }
+    if(Servico.length <= 0){
+        localStorage.removeItem("localemServico");
+        document.location.reload(true);
+    }
+}
+
+//abre coluna para inserir local com opções de salvar ou cancelar.
 function chamar(idchamada){
     document.body.style="overflow: hidden;";
     let exibirJanela = document.getElementById("containerChamar")
@@ -87,7 +108,6 @@ function chamar(idchamada){
     let nomeChamado = Disponiveis[posicaochamada].nome;
     let idchamadamod = idchamada + 1;
     document.getElementById("dadosChamar").innerHTML = `<p>Funcionario: ${nomeChamado}</p>` + '<p>Insira o Endereço: </p>'+`<input id="${idchamadamod}" class="inputInserirDados" type="text">` + '<br>' + `<button id="salvarChamarLocal" class="chamar" onclick="salvarChamar(${idchamadamod})">` + 'Salvar</button>' + `<button id="cancelarChamarLocal" class="excluir" onclick="cancelarChamar(${idchamadamod})"">`+ 'Cancelar</button>';
-    console.log(nomeChamado, idchamadamod);
 }
 
 //Salva as informações inseridas e recupera o nome para armazenar na coluna de funcionarios em serviço
@@ -97,18 +117,17 @@ function salvarChamar(iddochamado){
     let posicaochamada = contadordeposicao(iddochamado - 1);
     let nomeChamado = Disponiveis[posicaochamada].nome;
 
-    //deletar(iddochamado - 1)
-    //document.getElementById('filaServico').innerHTML += `<tr id="${iddochamado}">` + `<td>${nomeChamado}</td>`+ `<td>${local}</td>` + '<td>' + `<button style="width:58px;" class="chamar" onclick="">Fila</button>` +'</td>' + '<td>' +`<button nome="Excluir" class="excluir" onclick="deletarAtivo(${iddochamado})">Excluir</button>` + "</td>" +'</tr>';
+    Servico.push({id:iddochamado, nome:nomeChamado, local:local})
+    ServicoString = JSON.stringify(Servico)
+    localStorage.setItem("localemServico", ServicoString);
+    listarServico();
+//esse trecho apaga a janela de inserção de dados.
     deletar(iddochamado - 1);
     document.body.style="overflow: auto;";
     let exibirJanela = document.getElementById("containerChamar")
     exibirJanela.style="display: none;"
-
-    Servico.push({id:iddochamado, nome:nomeChamado, local:local})
-    ServicoString = JSON.stringify(Servico)
-    localStorage.setItem("emServico", ServicoString);
-    listarServico();
 }
+
 
 function cancelarChamar(){
     document.body.style="overflow: auto;";
@@ -125,22 +144,38 @@ function deletar(del){
     document.getElementById(del).remove();
     let DisponiveisString = JSON.stringify(Disponiveis)
     localStorage.setItem("disponiveis", DisponiveisString);
-    if(Disponiveis.length == 0){
-        document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Lista Vazia' +'</tr>';
+    if(Disponiveis.length < 1){
+        localStorage.removeItem("disponiveis");
+        document.location.reload(true);
+    }
+}
+function deletarAtivo(delservico){
+    let deletarPosicao = contadordeposicaoServico(delservico);
+    Servico.splice(deletarPosicao, 0);
+
+    Servico.splice(deletarPosicao, 1);
+    document.getElementById(delservico).remove();
+    let ServicoString = JSON.stringify(Servico)
+    localStorage.setItem("localemServico", ServicoString);
+    if(Servico.length <= 0){
+        localStorage.removeItem("localemServico");
+        document.location.reload(true);
     }
 }
 
 //remove toda a tabela e limpa o localstorage, exibe um alerta antes de continuar.
-function limparFila(){
-
-    var confirmarExclusao = confirm(" ATENÇÃO!\nNão será possível recuperar a fila, tem certeza que deseja apagar?");
-    if(confirmarExclusao == true){
-        //while evita de quando adicionar novo dado recupere os dados já excluidos.
-        while(Disponiveis.length > 0) {
-            Disponiveis.pop();
-         }
+function limparFilaEspera(j){
+    let confirmarExclusao = confirm(" ATENÇÃO!\nNão será possível recuperar a fila, tem certeza que deseja apagar?");
+    if(j == 1 && confirmarExclusao == true){
+        //splice evita que quando adicionar novo dado recupere os dados já excluidos.
+        Disponiveis.splice(0, Disponiveis.length);
         localStorage.removeItem("disponiveis");
-        document.getElementById("filaEspera").innerHTML = '<tr>'+'<td colspan="4"> Lista Vazia' +'</tr>';
+        document.location.reload(true);
+    }else if(j == 2 && confirmarExclusao == true){
+
+        Servico.splice(0, Servico.length);
+        localStorage.removeItem("localemServico");
+        document.location.reload(true);
     }else{
         alert("Ação Cancelada")
     }
@@ -150,6 +185,16 @@ function limparFila(){
 function contadordeposicao(idposicao){
     let contador;
     let pos = Disponiveis.map(function(e) { return e.id; });
+    for(let z = 0; z < pos.length; z++){
+        if(pos[z] == idposicao){
+            contador = z;
+        }
+    }
+    return contador
+}
+function contadordeposicaoServico(idposicao){
+    let contador;
+    let pos = Servico.map(function(e) { return e.id; });
     for(let z = 0; z < pos.length; z++){
         if(pos[z] == idposicao){
             contador = z;
